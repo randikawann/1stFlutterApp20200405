ba2f7c77fc17d7bebf9fea2f8ff125ac1ce8998d
Randika Wanninayaka <randikawann@gmail.com>
2020-04-05 13:57:10 +0530

    added profile with stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,71 @@ class RanCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/defaultperson.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
             Text(
               "NAME",
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
-            )
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              "Randika Wanninayaka",
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              "Current Level",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              "8",
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'randikawann@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
