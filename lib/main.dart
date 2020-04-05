161acde095992d579a79110589b230e953dc85a9
Randika Wanninayaka <randikawann@gmail.com>
2020-04-05 14:43:50 +0530

    added quotelist and screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,45 @@ class QuoteList extends StatefulWidget {
   _QuoteListState createState() => _QuoteListState();
 }
 
+
 class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genious',
+    'The trust is rarely pure and never simple'
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesom quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quotes) => Text(quotes) ).toList(),
+      ),
+    );
   }
 }
+
+//----------------------------------- Extra -----------------------------------
+//this is for the functions
+/*
+  body: Column(
+    children: quotes.map((quotes) {
+      return Text(quotes);
+    }).toList(),
+),
+--------------------------------------------------------------------------
+  it is convert as arrow funtion
+
+  body: Column(
+        children: quotes.map((quotes) => Text(quotes) ).toList(),
+      ),
+
+*/
