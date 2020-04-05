74b7758ccf90017d6abcb04e352d8271e22c8d7b
Randika Wanninayaka <randikawann@gmail.com>
2020-04-05 14:26:16 +0530

    start as new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: RanCard(),
+  home: QuoteList(),
 ));
 
-class RanCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _RanCardState createState() => _RanCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _RanCardState extends State<RanCard> {
-
-  int currentlevel = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text("First Flutter Application"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            currentlevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/defaultperson.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-              "Randika Wanninayaka",
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2.0,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Text(
-              "Current Level",
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-              '$currentlevel',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2.0,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  'randikawann@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-
-//create statefull widgets -> stfull
-class Test extends StatefulWidget {
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
+class _QuoteListState extends State<QuoteList> {
   @override
   Widget build(BuildContext context) {
     return Container();
