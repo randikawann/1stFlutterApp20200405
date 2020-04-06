4841dfc0d82105896163ee997368fdad6d05fea7
Randika Wanninayaka <randikawann@gmail.com>
2020-04-06 09:32:11 +0530

    added three screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,9 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'pages/home.dart';
+
 
 void main() => runApp(MaterialApp(
-  home: QuoteList(),
+  home: Home(),
 ));
 
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-
-class _QuoteListState extends State<QuoteList> {
-
-
-  List<Quote> quotes = [
-    Quote(author: 'osca wile', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'oscsssa wilecdc', text: 'I have nothing to declare except my genious'),
-    Quote(author: 'oscascddc wilecd', text: 'The trust is rarely pure and never simple')
-
-  ];
-
-  /* no need this widget because of Quote card class can be access directly....
-  Widget quoteTemplate(quote){
-    // from Flutter Outline rightclick "Card" select "extract widget" change name.
-    return QuoteCard(quote: quote);
-  }
-*/
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesom quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-          quote:  quote,
-          delete: (){
-            setState(() {
-              quotes.remove(quote);
-            });
-          }
-        ) ).toList(),
-
-      ),
-    );
-  }
-}
 
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class choose_location extends StatefulWidget {
+  @override
+  _choose_locationState createState() => _choose_locationState();
+}
+
+class _choose_locationState extends State<choose_location> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Choose Location screen')),
+    );
+  }
+}
