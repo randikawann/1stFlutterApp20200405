efe7efbf48c889b0f79f3e4b6de43df8341df8ea
Randika Wanninayaka <randikawann@gmail.com>
2020-04-06 08:13:54 +0530

    change quoteTemplate to Quotecard class

--- lib/main.dart
+++ lib/main.dart
@@ -22,32 +22,8 @@ class _QuoteListState extends State<QuoteList> {
   ];
 
   Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              "cdc",
-              style: TextStyle(
-                fontSize: 10.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0,),
-            Text(
-              'ddffdfdf',
-              style: TextStyle(
-                fontSize: 10.0,
-                color: Colors.grey[600],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    // from Flutter Outline rightclick "Card" select "extract widget" change name.
+    return QuoteCard(quote: quote);
   }
 
 
@@ -63,6 +39,41 @@ class _QuoteListState extends State<QuoteList> {
       body: Column(
         children: quotes.map((quote) => quoteTemplate(quote) ).toList(),
 
+      ),
+    );
+  }
+}
+
+class QuoteCard extends StatelessWidget {
+  Quote quote;
+  QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
