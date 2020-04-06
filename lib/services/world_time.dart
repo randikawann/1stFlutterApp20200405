3c3d72503823d0b0bcd22bfe3f5f26e6972964dc
Randika Wanninayaka <randikawann@gmail.com>
2020-04-06 13:48:15 +0530

    added try catch block o world_time class

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -16,24 +16,30 @@ class WorldTime {
 
 
   Future<void> getTime() async{
-//    baseurl = http://worldtimeapi.org/api/timezone/Europe/London
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-//    print(response.body);
-    // import 'dart:convert'; package
-    Map data = jsonDecode(response.body);
 
-    //get properties from data
-    String datetime = data['utc_datetime'];
+    try{
+      //    baseurl = http://worldtimeapi.org/api/timezone/Europe/London
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+//    print(response.body);
+      // import 'dart:convert'; package
+      Map data = jsonDecode(response.body);
+
+      //get properties from data
+      String datetime = data['utc_datetime'];
 //    String off_set = data['utc_offset']; // --- +01:00
-    String off_set = data['utc_offset'].substring(1,3); // --- 01
+      String off_set = data['utc_offset'].substring(1,3); // --- 01
 //    print('$off_set');
 
-    // create date time object
-    DateTime now = DateTime.parse(datetime); // --- 2020-04-06 07:23:37.725864Z
-    now = now.add(Duration(hours: int.parse(off_set))); // --- 2020-04-06 08:28:28.952756Z
+      // create date time object
+      DateTime now = DateTime.parse(datetime); // --- 2020-04-06 07:23:37.725864Z
+      now = now.add(Duration(hours: int.parse(off_set))); // --- 2020-04-06 08:28:28.952756Z
 
-    //set the time property
-    time = now.toString();
+      //set the time property
+      time = now.toString();
+
+    }catch (e){
+      time = "Could not load...";
+    }
   }
 
 
