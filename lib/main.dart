e04dde1c1f7cd1ec15e6076bff41d83e94dfcc0d
Randika Wanninayaka <randikawann@gmail.com>
2020-04-06 11:05:38 +0530

    example about setstate function and async and await functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/choose_location.dart';
 
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+//  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChoooseLocation(),
+  },
 ));
 
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_first/main.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,7 +13,19 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("World Map"),
       ),
-      body: Center(child: Text("Home Screen")),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            FlatButton.icon(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text("Edit Location"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
